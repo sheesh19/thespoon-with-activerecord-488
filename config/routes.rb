b5e5d25ddfbb7746883c13e599325d53cc3cdbec
Sheila Leveille <sbleveille@gmail.com>
2020-11-16 18:55:23 -0500

    FEAT: added crud routes to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # VERB + URL/PATH + CONTROLLER#ACTION
+
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant # new route is for our new form view
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants # => creates all CRUD routes for us
 end
